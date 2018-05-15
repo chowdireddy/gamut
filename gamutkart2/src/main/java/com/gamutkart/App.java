48bc81df9e82a926ac9da3cd0b8f9df36f4f0474
chowdireddy <chowdireddy2006@gmail.com>
2018-05-15 21:50:04 +0530

    im adding new file to repo

---
+++ gamutkart2/src/main/java/com/gamutkart/App.java
@@ -0,0 +1,18 @@
+package com.gamutkart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int i;
+		for(i=0;i<=1000;i++)
+		{
+			i += 3;
+        	System.out.println( "print number: " + i);
+   		}
+	 }
+}
